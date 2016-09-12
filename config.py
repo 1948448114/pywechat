7f3cb9d3f576269c12b5e7b5dc34a3740721895f
maxhh <490949611@qq.com>
2016-09-12 17:20:55 +0800

    访问不到api程序

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 LOCAL = u'http://123.57.221.18/wechata'
 URL = u'http://123.57.221.18/wechata'
@@ -15,9 +14,4 @@ loan2_url=u"http://mp.weixin.qq.com/s?__biz=MzAxMzExMTAwMw==&amp;mid=507813593&a
 
 greenroad_pic_url=u"http://mmbiz.qpic.cn/mmbiz_png/0LVWrbWYM168cPDvHDEfBK3bjCTa1Er6OkggIIdxQenSia5I5gGUDZceawKX8lggleZArOZGcAg4Q2rS54ibrgkA/0?wx_fmt=png"
 greenroad_url=u"http://mp.weixin.qq.com/s?__biz=MzAxMzExMTAwMw==&amp;mid=507813596&amp;idx=2&amp;sn=3215a14b59ea59421f0131edb902b573#rd"
-=======
-# -*- coding: utf-8 -*-
-LOCAL = u'http://123.57.221.18/wechata'
-URL = u'http://123.57.221.18/wechata'
-ResponseContent = u'您好，欢迎关注东南大学学生事务服务中心！ 您可以直接回复“提问+您的问题”我们将为您解答，答疑时间为周一至周五09：00-17：00. 东南大学学生事务服务中心联系方式：025-52090282，九龙湖校区大活524。'
->>>>>>> origin/master
+
